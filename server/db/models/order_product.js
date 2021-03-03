8b2af311e9e909667709de75bd0b4a9aae5a040b
Brittany Fenney <brittanyfenney@gmail.com>
2021-03-03 14:40:38 -0700

    Fix: db schema

---
+++ server/db/models/order_product.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Order_Product = db.define('order_product', {
+  quantity: {
+    type: Sequelize.INTEGER
+  },
+  price: {
+    type: Sequelize.INTEGER
+  }
+})
+
+module.exports = Order_Product
